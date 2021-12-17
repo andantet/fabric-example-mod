4368dc77b18199084f6f3a3e4c8f8bc3d4f3a88f
Daniel Orr <orrdaniel73@gmail.com>
2021-12-17 12:18:41 +0000

    remove datagen

--- src/main/java/me/andante/example/datagen/impl/DataCacheAccess.java
+++
@@ -1,9 +0,0 @@
-package me.andante.example.datagen.impl;
-
-import java.nio.file.Path;
-
-@SuppressWarnings("unused")
-public interface DataCacheAccess {
-    void addCopyPath(Path out);
-    void keepFiles(Path... keep);
-}
